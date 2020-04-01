cd9abb27abe67e5776df3d8f631739bb13217d2b
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-04-01 18:43:35 +0300

    Feature insert sort

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -27,4 +27,26 @@ public class Main {
         arr[j] = tmp;
     }
 
+    /**
+     *
+     * @param arr
+     */
+    private static void sortInsert(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
