171faecc146339f235221d293a3ff842ddee79c1
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-04-01 18:38:53 +0300

    Feature bubble sort

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -11,4 +11,20 @@ public class Main {
         System.out.println("TEST");
     }
 
+    private static void sortBubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
