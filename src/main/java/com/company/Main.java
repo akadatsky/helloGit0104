42874294b259a561ae0b16053a688a57bbf58f1e
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-04-01 18:29:21 +0300

    Add random

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
+
+    private static Random random = new Random();
 
     // test
     public static void main(String[] args) {
