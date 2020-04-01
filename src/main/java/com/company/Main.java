8ecbf26ef13501de18d66bc79af0e2d5a4f722cc
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-04-01 18:50:30 +0300

    implement quick sort

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -11,4 +11,31 @@ public class Main {
         System.out.println("TEST");
     }
 
+    private static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                exchange(arr, i, j);
+            }
+        }
+        exchange(arr, end, i + 1);
+        return i + 1;
+    }
+
+    private static void exchange(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
