24226781d62cdcf18729234158a3d66b9bf4e29f
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-04-01 18:40:33 +0300

    implement swap

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -23,4 +23,10 @@ public class Main {
         }
     }
 
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
